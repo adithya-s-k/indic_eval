afe1d5c6ef3b7f11378cdb6eda857b6f31a248f5
Adithya S K <adithyaskolavi@gmail.com>
2024-04-04 01:33:41 +0530

    added email push to leaderboard feature - Adithya S K

--- src/indic_eval/logging/evaluation_tracker.py
+++ src/indic_eval/logging/evaluation_tracker.py
@@ -108,6 +108,7 @@ class EvaluationTracker:
         push_results_to_hub: bool,
         push_details_to_hub: bool,
         public: bool,
+        push_to_leaderboard: str,
         push_results_to_tensorboard: bool = False,
     ) -> None:
         """Saves the experiment information and results to files, and to the hub if requested.
@@ -153,6 +154,7 @@ class EvaluationTracker:
             "config_tasks": self.task_config_logger.tasks_configs,
             "summary_tasks": self.details_logger.compiled_details,
             "summary_general": asdict(self.details_logger.compiled_details_over_all_tasks),
+            "email": push_to_leaderboard,
         }
         dumped = json.dumps(to_dump, cls=EnhancedJSONEncoder, indent=2)
 
